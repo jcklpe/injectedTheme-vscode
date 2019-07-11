2f91fa300e4df2da2171372e2ee8e589892a403a
jcklpe <david@jackalope.tech>
2019-07-11 01:56:24 -0500

    fixed gulp build and updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,21 @@
 // Gulp.js configuration
 "use strict";
 
-
 //source and build folders
 const dir = {
-  src: "./assets/",
-  build: "./",
-  root: "./"
-},
+    src: "./assets/",
+    build: "./",
+    root: "./"
+  },
   // Gulp and plugins
   gulp = require("gulp"),
   gutil = require("gulp-util"),
-  newer = require("gulp-newer"),
-  imagemin = require("gulp-imagemin"),
   sass = require("gulp-sass"),
   postcss = require("gulp-postcss"),
-  deporder = require("gulp-deporder"),
-  concat = require("gulp-concat"),
-  stripdebug = require("gulp-strip-debug"),
-  uglify = require("gulp-uglify"),
-  sourcemaps = require("gulp-sourcemaps"),
-  gulpImport = require("gulp-imports");
+  sourcemaps = require("gulp-sourcemaps");
 
 // Browser-sync
-const browsersync = false;
-
+var browsersync = false;
 
 //- CSS
 //config
@@ -41,118 +32,43 @@ var css = {
   processors: [
     require("postcss-assets")({
       // loadPaths: ['images/'],
-      basePath: dir.build,
+      basePath: dir.build
       // baseUrl: "/wp-content/themes/jackalope/"
     }),
-    require("autoprefixer")({
-      browsers: ["last 2 versions", "> 2%"]
-    }),
+    require("autoprefixer")(),
     require("css-mqpacker"),
     require("cssnano")
   ]
 };
 
 // CSS processing
-gulp.task("css", () =>
-{
-  return gulp
-    .src(css.src)
-    .pipe(sourcemaps.init())
-    .pipe(sass(css.sassOpts))
-    .pipe(postcss(css.processors))
-    .pipe(sourcemaps.write("./"))
-    .pipe(gulp.dest(css.build))
-    .pipe(
-      browsersync
-        ? browsersync.reload({
-          stream: true
-        })
-        : gutil.noop()
-    );
-});
-
-//-Javascript
-//config
-const jshead = {
-  src: dir.src + "js/head.js", //*/
-  build: dir.build + "./",
-  watch: dir.src + "js/**/*.js" //*/
-};
-
-gulp.task("jshead", function ()
-{
-  return gulp
-    .src(jshead.src)
-    .pipe(sourcemaps.init())
-    .pipe(gulpImport())
-    .pipe(uglify())
-    .pipe(stripdebug())
-    .pipe(sourcemaps.write("./"))
-    .pipe(gulp.dest(jshead.build))
-    .pipe(
-      browsersync
-        ? browsersync.reload({
-          stream: true
-        })
-        : gutil.noop()
-    );
-});
-
-const jsfooter = {
-  src: dir.src + "js/footer.js", //*/
-  build: dir.build + "./",
-  watch: dir.src + "js/**/*.js" //*/
-};
-
-gulp.task("jsfooter", function ()
-{
-  return gulp
-    .src(jsfooter.src)
-    .pipe(sourcemaps.init())
-    .pipe(gulpImport())
-    .pipe(uglify())
-    .pipe(stripdebug())
-    .pipe(sourcemaps.write("./"))
-    .pipe(gulp.dest(jsfooter.build))
-    .pipe(
-      browsersync
-        ? browsersync.reload({
-          stream: true
-        })
-        : gutil.noop()
-    );
-});
-
-gulp.task("js", ["jshead", "jsfooter"]);
-
-//- Build
-gulp.task("build", ["php", "css", "js"]);
+gulp.task(
+  "css",
+  gulp.series(() => {
+    return gulp
+      .src(css.src)
+      .pipe(sourcemaps.init())
+      .pipe(sass(css.sassOpts))
+      .pipe(postcss(css.processors))
+      .pipe(sourcemaps.write("./"))
+      .pipe(gulp.dest(css.build))
+      .pipe(
+        browsersync
+          ? browsersync.reload({
+              stream: true
+            })
+          : gutil.noop()
+      );
+  })
+);
 
 //- Watch
 
-gulp.task("watch", () =>
-{
-  gulp.watch(css.watch, ["css"]);
+gulp.task(
+  "watch",
+  gulp.series("css", done => {
+    gulp.watch(css.watch, gulp.series("css"));
 
-  //gulp.watch(php.src, ["php"], browsersync ? browsersync.reload : {});
-
-  gulp.watch(jshead.watch, ["jshead"]);
-
-  gulp.watch(jsfooter.watch, ["jsfooter"]);
-});
-
-// default task
-gulp.task("default", ["build", "watch"]);
-
-//-Image Uploads optimization
-
-gulp.task("image", () =>
-  gulp
-    .src("../../uploads/**/*")
-    .pipe(
-      imagemin({
-        verbose: true
-      })
-    )
-    .pipe(gulp.dest("../../uploads/"))
+    done();
+  })
 );
